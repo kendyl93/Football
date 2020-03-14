e82e4c82091c6800cc75ba5a2b3636cbaca55c38
Pawel Stanecki <p.stanecki93@gmail.com>
2020-03-14 20:05:32 +0100

    Remove unused code

--- src/views/Matches/Matches.js
+++ src/views/Matches/Matches.js
@@ -2,11 +2,7 @@ import React from 'react';
 
 import Match from './Match';
 
-const byCompetition = (a, b) => a.competition.id - b.competition.id;
-
 const Matches = ({ matches }) => {
-  // const sorted = matches.sort(byCompetition);
-
   return (
     matches.length > 0 &&
     matches.map(({ id, homeTeam, awayTeam } = {}) => (
